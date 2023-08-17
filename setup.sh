a0e1753ffa428a224a7cdae0c4a8d70961f4e497
Ben Ostrowski <benjamin.h.ostrowski@gmail.com>
2023-08-17 11:30:26 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# Make sure you've cloned this repo or copied the files from it to your local folder.
+
+## Chocolatey packages install (you need chocolatey first, just go to their website)
+choco install packates.config
+
